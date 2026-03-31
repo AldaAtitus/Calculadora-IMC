437a5f2ac864b5d3c9515160052e6fcc35958bd3
AldaAtitus <1137226@atitus.edu.br>
2026-03-30 22:01:21 -0300

    ajustes3

--- Imc.java
+++ Imc.java
@@ -2,7 +2,7 @@ void main(){
     IO.println("=== Calculo de IMC ===");
     String nome = IO.readln("Informe seu nome: ");
     char genero = IO.readln("Informe seu gênero (M: masculino, F: feminino, N: prefiro não informar)").charAt(0);
-    genero = Character.toUpperCase(genero); // transforma em maiúsculo
+    genero = Character.toUpperCase(genero);
     double altura = Double.parseDouble(IO.readln("Informe sua altura (em metros): "));
     double peso = Double.parseDouble(IO.readln("Informe seu peso (em quilos): "));
     double imc = peso / (altura * altura);
